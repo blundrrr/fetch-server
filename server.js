7f32ec15ed3b3cf70cd8402dd7bf5376873d8449
blundrrr <marcodecillia58@gmail.com>
2025-01-27 01:12:57 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const port = 4000;
+
+
+app.use(cors());
+
+
+app.get("/api/nfts/:wallet", async (req, res) => {
+  const walletAddress = req.params.wallet;
+  const apiUrl = `https://api-mainnet.magiceden.dev/v2/wallets/${walletAddress}/tokens`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    res.json(response.data); // Restituisce i dati ricevuti
+  } catch (error) {
+    console.error("Errore durante il recupero dei dati NFT:", error.message);
+    res.status(500).json({ error: "Errore nel recupero degli NFT" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server proxy in esecuzione su http://localhost:${port}`);
+});
